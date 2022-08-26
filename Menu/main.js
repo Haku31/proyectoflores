87be1b3b91a71a7627d2043773e9d02d129dda28
Alex <barretojhonalex@gmail.com>
2022-08-26 15:23:23 -0500

    menu y vista principal

---
+++ Menu/main.js
@@ -0,0 +1,50 @@
+let listElements = document.querySelectorAll('.list__button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+
+    })
+});
+document.querySelector(".addParam").addEventListener("click",addParam);
+document.querySelector(".showResults").addEventListener("click",showResults);
+
+var parametros = [];
+var valores = [];
+
+function addParam(){
+    let html = document.querySelector(".container").innerHTML;
+    let newHTML = '<div><input type="text" class="parametro" placeholder="parametro"><input type="number" class="valor" placeholder="valor"></div>'; 
+    document.querySelector(".container").innerHTML = html + newHTML;
+} 
+
+
+function showResults(){
+    for (var i = 0; i < document.querySelectorAll('.parametro').length ; i++) {
+      parametros.push(document.querySelectorAll('.parametro')[i].value);
+      valores.push(parseInt(document.querySelectorAll(".valor")[i].value));
+    }
+    var data = [{
+      x: parametros,
+      y: valores,
+      type: "linear"
+    }];
+    Plotly.newPlot("grafico",data);
+}
+// sidebar toggle
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
